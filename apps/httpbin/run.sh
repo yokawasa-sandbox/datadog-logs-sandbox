a538a60fa2d31a02f197af22d9f9133295974dbc
Yoichi Kawasaki <yokawasa@gmail.com>
2024-09-01 08:07:52 +0900

    add ref for enabling request logging
    Signed-off-by: Yoichi Kawasaki <yokawasa@gmail.com>

--- apps/httpbin/run.sh
+++ apps/httpbin/run.sh
@@ -5,4 +5,5 @@
 # docker run -d -p 8080:80 httpbin
 
 ## Enabling request logging
+# ref: https://github.com/postmanlabs/httpbin/issues/421
 docker run -p 8080:80 -e GUNICORN_CMD_ARGS="--capture-output --error-logfile - --access-logfile - --access-logformat '%(h)s %(t)s %(r)s %(s)s Host: %({Host}i)s}'" httpbin
